56e1ee8372a758a775010ad6ece698e6e9b4de7e
jamesCarl1204 <jamesmagsipoc887@gmail.com>
2026-08-27 23:25:48 +0800

    fix the code make shorter

--- projects/password-generator/script.js
+++ projects/password-generator/script.js
@@ -1,120 +1,50 @@
 
-
-
-
 function generatePassword() {
       
-      let input = document.getElementById("inputNum").value
-      let numberRad = document.getElementById("numRad");
-      let upperCaseRad =  document.getElementById("upperCaseRad")
-      let lowerCaseRad = document.getElementById("lowerCaseRad")
+      let input =  Number(document.getElementById("inputNum").value)
+      let includeNumber = document.getElementById("numRad").checked;
+      let includeUpperCase =  document.getElementById("upperCaseRad").checked
+      let includeLowerCase = document.getElementById("lowerCaseRad").checked
       let result =  document.getElementById("result")
     
      const upperCase = "ABCDEFGHIJKLMNOPQRSTUBWXYZ";
      const lowerCase = upperCase.toLowerCase();
      const number = "1234567890"
       
-     let allowedChars = "";
-     let password = ""; 
+       let includedTypes = [];
 
-     let includeLowerCase = lowerCaseRad.checked;
-     let includeUpperCase = upperCaseRad.checked;
-     let includeNumber = numberRad.checked;
+       if(includeNumber) {includedTypes.push(number)}
+       if(includeUpperCase) {includedTypes.push(upperCase)}
+       if(includeLowerCase) {includedTypes.push(lowerCase)}
 
-     input = Number(input)
-      
-
-     if(includeLowerCase && includeUpperCase && includeNumber && input >= 8) {
        
-        allowedChars += upperCase + lowerCase +  number;
-        let length = input
+
+     if(input < 8 || includedTypes.length < 2 ) {
+        result.textContent = "Pick 2 types and length should be 8 or higher"
+        return
+     }
+
+     let passwordChars =  []
+     let allowedChars = includedTypes.join("")
+     
         
-        for(let i = 0; i < length; i++) {
-            if(i === 0) {
-                const randIndex = Math.floor(Math.random() * upperCase.length)
-                password += upperCase[randIndex]
-            }
-            if(i === 1) {
-                const randIndex = Math.floor(Math.random() * lowerCase.length)
-                password += lowerCase[randIndex]
-            }
-            if(i === 2) {
-                const randIndex = Math.floor(Math.random() * number.length)
-                password += number[randIndex]
-            }
-            if(i > 2) {
-             const randIndex = Math.floor(Math.random() * allowedChars.length)
-             password += allowedChars[randIndex]
-            }
-     }
-      result.textContent = password
-     }
-     else if(includeLowerCase && includeUpperCase && input >= 8 ) {
-        allowedChars += upperCase + lowerCase;
-        let length = input
+     includedTypes.forEach(includedType => {
+        const randIndex = Math.floor(Math.random() * includedType.length);
+        passwordChars.push(includedType[randIndex])
+     })
 
-         for(let i = 0; i < length; i++) {
-            if(i === 0) {
-                const randIndex = Math.floor(Math.random() * upperCase.length)
-                password += upperCase[randIndex]
-            }
-            if(i === 1) {
-                const randIndex = Math.floor(Math.random() * lowerCase.length)
-                password += lowerCase[randIndex]
-            }
-           
-            if(i > 1) {
-             const randIndex = Math.floor(Math.random() * allowedChars.length)
-             password += allowedChars[randIndex]
-            }
-         }
-         result.textContent = password;
-     }
-     else if(includeLowerCase && includeNumber && input >= 8) {
-        allowedChars += lowerCase + number;
-        let length = input
+     for(let i = passwordChars.length; i < input; i++) {
+        const randIndex = Math.floor(Math.random() * allowedChars.length);
+        passwordChars.push(allowedChars[randIndex])
+     } 
 
-         for(let i = 0; i < length; i++) {
-            if(i === 0) {
-                const randIndex = Math.floor(Math.random() * number.length);
-                password += number[randIndex]
-            }
+     for(let i = passwordChars.length -1;  i  >  0; i-- ) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [passwordChars[i], passwordChars[j] = passwordChars[j], passwordChars[i]]
+     } 
+     
+      let password = passwordChars.join("")
 
-            if(i === 1) {
-                const randIndex = Math.floor(Math.random() * lowerCase.length)
-                password += lowerCase[randIndex]
-            }
-           
-            if(i > 1) {
-             const randIndex = Math.floor(Math.random() * allowedChars.length)
-             password += allowedChars[randIndex]
-            }
-         }
-         result.textContent = password
-     }
-     else if(includeUpperCase && includeNumber && input >= 8) {
-        allowedChars += upperCase + number;
-        let length = input
-        
-          for(let i = 0; i < length; i++) {
-            if(i === 0) {
-                const randIndex = Math.floor(Math.random() * upperCase.length)
-                password += upperCase[randIndex]
-            }
-           
-            if(i === 1) {
-                const randIndex = Math.floor(Math.random() * number.length)
-                password += number[randIndex]
-            }
-            if(i > 1) {
-             const randIndex = Math.floor(Math.random() * allowedChars.length)
-             password += allowedChars[randIndex]
-            }
-         }
-         result.textContent = password       
-        }
-    else {
-        result.textContent = "Pick 2 for strong Password and length should be 8 or higher "
+     result.textContent = password
+     
     }
-
-}
